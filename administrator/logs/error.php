9b5cc825e3ff605fbb085042d918c1d118e4db43
DivineWisdom <didiwiseman@gmail.com>
2019-01-07 19:13:01 +0100

    A demo Joomla website built with the Helix3 template

---
+++ administrator/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-04-06 18:50:05 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-04-06T18:50:05+00:00	INFO ::1	cookiefailure	Empty password not allowed.
+2019-04-06T18:50:09+00:00	INFO ::1	updater	Loading information from update site #1 with name "Joomla! Core" and URL https://update.joomla.org/core/list.xml took 2.07 seconds
